4f91d8445f5279e73a639d99399d31a4c91921eb
Krzysztof Langner <klangner@gmail.com>
2018-11-17 15:33:56 +0100

    Initialize static values

--- app/src/main.rs
+++ app/src/main.rs
@@ -7,8 +7,14 @@ use rt::entry;
 
 entry!(main);
 
-pub fn main() -> ! {
-    let _x = 42;
+static RODATA: &[u8] = b"Hello, world!";
+static mut BSS: u8 = 0;
+static mut DATA: u16 = 1;
+
+fn main() -> ! {
+    let _x = RODATA;
+    let _y = unsafe { &BSS };
+    let _z = unsafe { &DATA };
 
     loop {}
 }
